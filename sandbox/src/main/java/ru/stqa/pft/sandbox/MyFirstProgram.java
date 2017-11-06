30d050ed1499ca9d2c3d8569ec13acd15d124f73
Vlad Chepish <vladforwork@yandex.ru>
2017-11-06 12:17:59 +0300

    added a fuw functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram{
 
 	public static void main (String[] args) {
+		hello("world");
+		hello("Vlad");
+		hello("user");
 
-		System.out.println("Hello, World!");
+		double len = 5;
+		System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+		double a = 6;
+		double b = 8;
+		System.out.println("Площадь прямолгольника со сторонами " + a + " и " + b + " = " + area(a,b));
 	}
+
+	public static void hello(String somebody) {
+
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double l){
+		return l * l;
+	}
+
+	 public static double area(double a, double b){
+		return a * b;
+	 }
 }
