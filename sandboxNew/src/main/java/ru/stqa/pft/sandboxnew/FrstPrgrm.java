def22cb9489191eef1ade9553750b06c2763f024
Vladislav Chepish <vladforwork@yandex.ru>
2017-11-25 21:29:06 +0300

    Добавлены функции из урока

--- sandboxNew/src/main/java/ru/stqa/pft/sandboxnew/FrstPrgrm.java
+++ sandboxNew/src/main/java/ru/stqa/pft/sandboxnew/FrstPrgrm.java
@@ -4,8 +4,32 @@ public class FrstPrgrm{
 
 	public static void main(String[] args){
 
-		System.out.println("Hello, World!");
-	
+		hello("world");
+		hello("User");
+		hello("Vlad");
+
+		double len = 5;
+		System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+		double a = 5;
+		double b = 18;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+
+
+	}
+
+	public static void hello(String somebody){
+
+		System.out.println("Hello," + somebody + "!");
+
+	}
+
+	public static double area (double l){
+		return l * l;
+	}
+
+	public static double area (double a, double b){
+		return a * b;
 	}
 
 }
