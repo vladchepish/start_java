dc904cdba48762776cf3f1f71947f7cf9597f78a
Vladislav Chepish <vladforwork@yandex.ru>
2017-12-04 01:06:17 +0300

    добавлен HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -3,21 +3,15 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SessionHelper {
-
-    FirefoxDriver wd;
+public class SessionHelper extends HelperBase {
 
     public SessionHelper (FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void login(String login, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(login);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        type(By.name("user"), login);
+        type(By.name("pass"), password);
+        click(By.xpath("//form[@id='LoginForm']/input[3]"));
     }
 }
