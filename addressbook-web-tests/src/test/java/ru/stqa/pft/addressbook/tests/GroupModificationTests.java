355a1d6c6cd8d3f25505409a0999afbf613a1a6d
Vladislav Chepish <vladforwork@yandex.ru>
2017-12-19 14:51:35 +0300

    Доработан тест для модификации группы - получаем и сравниваем множества

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -17,10 +18,15 @@ public class GroupModificationTests extends TestBase {
         List<GroupDate> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillingGroupForm(new GroupDate("newTestName", "newTestHeader", "newTestFooter"));
+        GroupDate group = new GroupDate(before.get(before.size() - 1).getId(),"newTestName", "newTestHeader", "newTestFooter");
+        app.getGroupHelper().fillingGroupForm(group);
         app.getGroupHelper().submitGroupMoodification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupDate> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() );
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
