0dca8b96bfff15cc05f45f16459563c21995b61d
Vladislav Chepish <vladforwork@yandex.ru>
2017-12-19 23:09:38 +0300

    Цикл заменён на лямбда функцию

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreatorTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreatorTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,18 +14,12 @@ public class GroupCreatorTests extends TestBase {
     public void testGroupCreator() {
         app.getNavigationHelper().goToGroup();
         List<GroupDate> before = app.getGroupHelper().getGroupList();
-        GroupDate group = new GroupDate("testName", null, null);
+        GroupDate group = new GroupDate("testName2", null, null);
         app.getGroupHelper().createGroup(group);
         List<GroupDate> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupDate g : after){
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
