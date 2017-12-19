e3685b919e64c5c8a615eaf5dd5d368b03f3aa70
Vladislav Chepish <vladforwork@yandex.ru>
2017-12-19 21:55:04 +0300

    Добавлена проверка в тест создания группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupDate.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupDate.java
@@ -1,51 +1,31 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupDate {
-    private final String id;
+
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupDate groupDate = (GroupDate) o;
-
-        if (id != null ? !id.equals(groupDate.id) : groupDate.id != null) return false;
-        return name != null ? name.equals(groupDate.name) : groupDate.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupDate{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    public GroupDate(String id, String name, String header, String footer) {
+    public GroupDate(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
+
     public GroupDate(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public String getId() {
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
         return id;
     }
 
@@ -59,6 +39,32 @@ public class GroupDate {
 
     public String getFooter() {
         return footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupDate{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupDate groupDate = (GroupDate) o;
+
+        if (id != groupDate.id) return false;
+        return name != null ? name.equals(groupDate.name) : groupDate.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreatorTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreatorTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreatorTests extends TestBase {
@@ -12,9 +13,20 @@ public class GroupCreatorTests extends TestBase {
     public void testGroupCreator() {
         app.getNavigationHelper().goToGroup();
         List<GroupDate> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupDate("testName", null, null));
+        GroupDate group = new GroupDate("testName", null, null);
+        app.getGroupHelper().createGroup(group);
         List<GroupDate> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        int max = 0;
+        for (GroupDate g : after){
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
