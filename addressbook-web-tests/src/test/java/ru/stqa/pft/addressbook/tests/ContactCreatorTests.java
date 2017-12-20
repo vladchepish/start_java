4d955441ab6b835ac98d883a119aec8d00e90f2f
Vladislav Chepish <vladforwork@yandex.ru>
2017-12-20 22:00:36 +0300

    цикл вычисления макисмального идентификатора заменён на лямбдафункцию

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreatorTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreatorTests.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactDate;
-import ru.stqa.pft.addressbook.model.GroupDate;
 
 import java.util.HashSet;
 import java.util.List;
@@ -14,18 +13,12 @@ public class ContactCreatorTests extends TestBase {
     public void ContactCreatorTests(){
         app.getNavigationHelper().goToHomePage();
         List<ContactDate> before = app.getContactHelper().getContactList();
-        ContactDate contact = new ContactDate("testFirstName1", "testMiddleName1", "testLastName1", "testAddress1", "+79797979797", "test@test.test", "testName");
+        ContactDate contact = new ContactDate("testFirstName2", "testMiddleName1", "testLastName1", "testAddress1", "+79797979797", "test@test.test", "testName");
         app.getContactHelper().creationContact(contact, true);
         List<ContactDate> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (ContactDate g : after) {
-            if (g.getId() > max){
-                max = g.getId();
-            }
-        }
-        contact.setId(max);
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
