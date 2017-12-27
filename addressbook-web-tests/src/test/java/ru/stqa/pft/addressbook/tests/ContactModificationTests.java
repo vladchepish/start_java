ef97869f0a3a6ca6acc9fce392328c7ef0052ca5
Vladislav Chepish <vladforwork@yandex.ru>
2017-12-28 00:42:38 +0300

    Внешне улучшены тесты по удалению контакта и группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,9 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.junit.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactDate;
+import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.Comparator;
 import java.util.List;
@@ -22,17 +25,15 @@ public class ContactModificationTests extends TestBase {
 
     @Test
     public void testContactModification(){
-        Set<ContactDate> before = app.contact().all();
+        Contacts before = app.contact().all();
         ContactDate modifiedContact = before.iterator().next();
         ContactDate contact = new ContactDate()
                 .withId(modifiedContact.getId()).withFirstName("name").withMiddleName("middle").withLastName("last").withAddress("sad").withPhone("23123124").witheMail("aas@as.as");
         app.contact().modify(contact);
-        Set<ContactDate> after = app.contact().all();
+        Contacts after = app.contact().all();
         Assert.assertEquals(after.size(), before.size());
 
-        before.remove(modifiedContact);
-        before.add(contact);
-        Assert.assertEquals(before, after);
+        MatcherAssert.assertThat(after, CoreMatchers.equalTo(before.withOut(modifiedContact).withAdded(contact)));
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,11 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.junit.MatcherAssert.*;
 
 public class GroupModificationTests extends TestBase {
 
@@ -20,17 +21,14 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupMoodification(){
 
-        Set<GroupDate> before = app.group().all();
+        Groups before = app.group().all();
         GroupDate modifiedGroup = before.iterator().next();
-        int index = before.size() - 1;
         GroupDate group = new GroupDate()
                 .withId(modifiedGroup.getId()).withName("newTestName").withHeader("newTestHeader").withFooter("newTestFooter");
         app.group().modify(group);
-        Set<GroupDate> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() );
+        Groups after = app.group().all();
+        assertThat(after.size(), equalTo( before.size() ));
 
-        before.remove(modifiedGroup);
-        before.add(group);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(before.withOut(modifiedGroup).withAdded(group)));
     }
 }
