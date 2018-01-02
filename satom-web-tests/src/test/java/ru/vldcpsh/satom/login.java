b10db5da32cd7f1dc1e7f3741467005a5cbac09a
Vlad Chepish <vladforwork@yandex.ru>
2018-01-02 17:06:55 +0300

    Добавлена песачница ещё одного проекта
    В директории Satom добавлен класс, который выполняет логин в сиситему, код ещё не разносился по вспомогательным классам и не преобразовывался во вспомогательные методы.

---
+++ satom-web-tests/src/test/java/ru/vldcpsh/satom/login.java
@@ -0,0 +1,53 @@
+package ru.vldcpsh.satom;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class login {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void login() {
+        wd.get("http://satom.ru/");
+        wd.findElement(By.linkText("Вход")).click();
+        wd.findElement(By.name("login")).click();
+        wd.findElement(By.name("login")).clear();
+        wd.findElement(By.name("login")).sendKeys("zivo@p33.org");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("123123");
+        wd.findElement(By.id("submit-login")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
