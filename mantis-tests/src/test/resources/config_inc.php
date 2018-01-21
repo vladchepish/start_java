cabaa960ad7f17686b1a8a20311b8321803bf52c
Vlad Chepish <vladforwork@yandex.ru>
2018-01-22 01:11:50 +0300

    Добавлен и подкорректирован файл, который будет подкладываться через ФТП для отключения капчи

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_signup_use_captcha = OFF;
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'Tf3R4hQB+tgIqfMBYLIQ0HalfSb55KcQ4PvJsbotRXA=';
