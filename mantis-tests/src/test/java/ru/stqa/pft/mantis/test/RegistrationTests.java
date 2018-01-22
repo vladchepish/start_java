f903fe2f4f2a0cdebd6c20f6a7169c14dab7c70d
Vlad Chepish <vladforwork@yandex.ru>
2018-01-22 22:30:03 +0300

    Сделан тест для проверки возможности регистрации

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/test/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/test/RegistrationTests.java
@@ -1,11 +1,51 @@
 package ru.stqa.pft.mantis.test;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase{
 
-    @Test
-    public void testRegistration(){
-        app.registration().start("user1", "user@localhost.localdomain");
+    @BeforeMethod
+    public void startMailServer(){
+        app.mail().start();
     }
+
+
+    @Test
+    public void testRegistration() throws IOException, MessagingException {
+        String email = "user111@localhost.localdomain";
+        String user1 = "user111";
+        String password = "password";
+        String realname = "Егорович";
+        app.registration().start(user1, email);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, password, realname);
+        assertTrue(app.newSession().login(user1, password));
+
+
+
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer(){
+        app.mail().stop();
+    }
+
 }
