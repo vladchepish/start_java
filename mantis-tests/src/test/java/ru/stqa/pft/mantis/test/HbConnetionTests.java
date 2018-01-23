7bd687ebef0b0c279fc0cbae78872d8ec4309b32
Vlad Chepish <vladforwork@yandex.ru>
2018-01-23 23:56:33 +0300

    Создан тест для проверки подключения к базе

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/test/HbConnetionTests.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.mantis.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.UsersDate;
+
+import java.util.List;
+
+public class HbConnetionTests {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+    @Test
+    public void testHbConnection(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<UsersDate> result = session.createQuery( "from mantis_user_table").list();
+        for ( UsersDate users : result ) {
+            System.out.println(users);
+        }
+        session.getTransaction().commit();
+        session.close();
+
+
+
+    }
+}
