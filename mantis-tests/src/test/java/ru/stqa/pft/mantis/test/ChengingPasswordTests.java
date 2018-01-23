94ce7496f5602b52d118966d8e10be896f31f671
Vlad Chepish <vladforwork@yandex.ru>
2018-01-23 21:56:12 +0300

    Начат тест для для изменения пароля

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/test/ChengingPasswordTests.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.mantis.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ChengingPasswordTests extends TestBase {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void startMailServer(){
+        app.mail().start();
+    }
+
+    @Test
+    public void testChangingPassword(){
+        app.session().enterToSystem();
+        /*Позже не помешает добавить сюда проверку, что мы залогинились, например по тому же отображению имени пользователя.
+        * Нежно будет написать метод, который проверяет на вебе наличие блока отображающего имя пользователя*/
+        app.session().goToUsersPage();
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer(){
+        app.mail().stop();
+    }
+}
