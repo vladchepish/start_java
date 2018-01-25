923439af3b39c8af830a6f2fc237450b265f7cc6
Vlad Chepish <vladforwork@yandex.ru>
2018-01-25 22:39:53 +0300

    Обновлён прекондишн, проверяющий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactDate;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupDate;
 import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.File;
@@ -19,8 +20,15 @@ public class ContactDeletionTests extends TestBase{
         File photo = new File("src/test/resources/avatar.png");
         app.goTo().homePage();
         if (app.db().contacts().size() == 0) {
-            app.contact().create(new ContactDate().withFirstName("testFirstName1").withHomePhone("11111").withMobilePhone("22222")
-                    .withWorkPhone("333333").inGroup(groups.iterator().next()).withPhoto(photo), true);
+            if (app.db().groups().size() == 0) {
+                app.goTo().groupPage();
+                app.group().create(new GroupDate().withName("testName"));
+                groups = app.db().groups();
+            }
+            app.contact().create(new ContactDate().withFirstName("testFirstName1").withMiddleName("testMiddleName").withLastName("testLastName")
+                    .withAddress("testAddress").withHomePhone("11111").withMobilePhone("22222").withWorkPhone("333333")
+                    .withEmail1("test@test.test").withEmail2("test@test2.test").withEmail3("test@test3.test")
+                    .inGroup(groups.iterator().next()).withPhoto(photo), true);
         }
     }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactDate;
+import ru.stqa.pft.addressbook.model.GroupDate;
 import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.File;
@@ -20,7 +21,14 @@ public class ContactPhoneTests extends TestBase {
         File photo = new File("src/test/resources/avatar.png");
         app.goTo().homePage();
         if (app.db().contacts().size() == 0) {
-            app.contact().create(new ContactDate().withFirstName("testFirstName1").withHomePhone("11111").withMobilePhone("22222").withWorkPhone("333333")
+            if (app.db().groups().size() == 0) {
+                app.goTo().groupPage();
+                app.group().create(new GroupDate().withName("testName"));
+                groups = app.db().groups();
+            }
+            app.contact().create(new ContactDate().withFirstName("testFirstName1").withMiddleName("testMiddleName").withLastName("testLastName")
+                    .withAddress("testAddress").withHomePhone("11111").withMobilePhone("22222").withWorkPhone("333333")
+                    .withEmail1("test@test.test").withEmail2("test@test2.test").withEmail3("test@test3.test")
                     .inGroup(groups.iterator().next()).withPhoto(photo), true);
         }
     }
