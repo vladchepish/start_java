2bc7c0558a77f956d7afbd33ac1d7cea7674d266
Vladislav Chepish <vladforwork@yandex.ru>
2018-01-28 16:07:29 +0300

    Сгенерированы помошники из описания сервиса

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp(){
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("92.38.111.201");
+        assertEquals(geoIP.getCountryCode(), "CZE");
+    }
+}
