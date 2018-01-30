5daaec36da1d27e5c8f3cfc8f5f0a33f6c6fbc3d
Vlad Chepish <vladforwork@yandex.ru>
2018-01-30 21:02:31 +0300

    Добавлен тест, который создает баг репорт

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/test/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/test/SoapTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.mantis.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
@@ -20,7 +22,12 @@ public class SoapTests extends TestBase{
     }
 
     @Test
-    public void testCreateIssue(){
+    public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+        Set<Project> projects = app.soapHelper().getProjects();
+        Issue issue = new Issue().withSummery("testSummery")
+                .withDescription("testDescription").withProject(projects.iterator().next());
+        Issue created = app.soapHelper().addIssue(issue);
+        Assert.assertEquals(issue.getSummery(), created.getSummery());
 
     }
 }
