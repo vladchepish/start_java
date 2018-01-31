95de3d8e968d9887e5cd6431a6d8f358858ed2b9
Vlad Chepish <vladforwork@yandex.ru>
2018-01-31 22:11:02 +0300

    В тест бейс добавлена функция выводящая на консоль сообщение, что тест проигнорен, если тикет в состоянии отличном от закрытого

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/test/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/test/TestBase.java
@@ -1,16 +1,33 @@
 package ru.stqa.pft.mantis.test;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+    private boolean isIssueOpen(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if(app.soapHelper().checkingIssueStatus(issueId)){
+            return false;
+        }
+        return true;
+    }
 
     @BeforeSuite
     public void setUp() throws Exception {
