e0d29eb6dcc2e71e1b40a9b26257839addda2e40
Vlad Chepish <vladforwork@yandex.ru>
2018-02-01 23:20:29 +0300

    Добавлена функция проверяющая статус тикета

--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
@@ -1,8 +1,25 @@
 package ru.stqa.pft.rest.tests;
 
+import org.testng.SkipException;
 import ru.stqa.pft.rest.appmanager.ApplicationManager;
+
+import javax.xml.rpc.ServiceException;
+import java.io.IOException;
 
 public class TestBase {
 
     protected static final ApplicationManager app = new ApplicationManager();
+
+    public void skipIfNotFixed(int issueId) throws IOException, ServiceException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+    private boolean isIssueOpen(int issueId) throws IOException, ServiceException {
+        if(app.restSession().getStatus(issueId)){
+            return false;
+        }
+        return true;
+    }
 }
