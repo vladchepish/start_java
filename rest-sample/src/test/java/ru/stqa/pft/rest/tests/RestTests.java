fb20706ae8a779c4a4b8f2e6ed5f875266d5d37a
Vlad Chepish <vladforwork@yandex.ru>
2018-02-01 23:18:31 +0300

    Перед выполнением теста проверяется статус тикета

--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestTests.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.rest.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.rest.model.Issue;
 
+import javax.xml.rpc.ServiceException;
 import java.io.IOException;
 import java.util.Set;
 
@@ -11,7 +12,8 @@ import static org.testng.Assert.assertEquals;
 public class RestTests extends TestBase{
 
     @Test
-    public void testCreateIssue() throws IOException {
+    public void testCreateIssue() throws IOException, ServiceException {
+        skipIfNotFixed(827);
         Set<Issue> oldIssues = app.restSession().getIssues();
         Issue newIssue = new Issue().withSubject("test Issue Chepish").withDescription("new test Issue");
         int issueId = app.restSession().createIssue(newIssue);
