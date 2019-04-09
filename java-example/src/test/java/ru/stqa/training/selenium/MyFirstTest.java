516827147145b0d40038a358e67891bf402ac28d
vladf <vladforwork@yandex.ru>
2019-04-09 22:21:57 +0300

    added test LoginIntoAdminPanel added TestBase class

--- java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,35 +1,15 @@
 package ru.stqa.training.selenium;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyFirstTest {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-
-    @Before
-    public void start() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
-    }
+public class MyFirstTest extends TestBase{
 
     @Test
     public void myFirstTest(){
         driver.get("https://www.google.com/");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("webdriver");
         driver.findElement(By.cssSelector("div.FPdoLc input[value='Поиск в Google']")).click();
-    }
-
-    @After
-    public void stop(){
-        driver.quit();
-        driver = null;
     }
 
 }
---
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -0,0 +1,25 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestBase {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
