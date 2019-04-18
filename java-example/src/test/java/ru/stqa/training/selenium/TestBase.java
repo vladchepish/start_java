e400c9b3d103540f7bcd467dd0433fa10d03e5c9
vladf <vladforwork@yandex.ru>
2019-04-18 21:04:33 +0300

    some experements with browser start

--- java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -4,7 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class TestBase {
 
@@ -13,9 +16,13 @@ public class TestBase {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        //driver = new InternetExplorerDriver();
+        driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 10);
+
     }
+
 
     @After
     public void stop(){
