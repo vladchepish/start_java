51f3761e40685ca18d3b16a41be925e45c1c028b
vladf <vladforwork@yandex.ru>
2019-05-09 22:02:45 +0300

    added EventFiringWebDriver

--- java-example/src/test/java/ru/stqa/training/selenium/tests/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/tests/TestBase.java
@@ -2,10 +2,14 @@ package ru.stqa.training.selenium.tests;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.stqa.training.selenium.peges.BasePage;
 
@@ -13,13 +17,31 @@ import java.io.*;
 
 public class TestBase {
 
-    protected WebDriver driver;
+    protected EventFiringWebDriver driver;
     protected WebDriverWait wait;
     public static String baseUrl;
 
+    public static class MyListener extends AbstractWebDriverEventListener{
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + " found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            System.out.println(throwable);
+        }
+    }
+
     @Before
     public void start() throws IOException {
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new MyListener());
         //driver = new InternetExplorerDriver();
         //driver = new FirefoxDriver();
         driver.manage().window().maximize();
