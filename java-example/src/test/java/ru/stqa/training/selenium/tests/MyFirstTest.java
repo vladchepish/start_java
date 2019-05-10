709be324a6d9b65ff283065099d89a60e1f5c577
vladf <vladforwork@yandex.ru>
2019-05-10 13:24:30 +0300

    some corrections

--- java-example/src/test/java/ru/stqa/training/selenium/tests/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/tests/MyFirstTest.java
@@ -7,9 +7,13 @@ public class MyFirstTest extends TestBase{
 
     @Test
     public void myFirstTest(){
-        driver.get("https://www.google.com/");
-        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("webdriver");
-        driver.findElement(By.cssSelector("div.FPdoLc input[value='Поиск в Google']")).click();
+        //driver.get("https://www.google.com/");
+        driver.get("https://www.seleniumhq.org");
+        System.out.println(driver.manage().logs().getAvailableLogTypes());
+        driver.manage().logs().get("browser").forEach(l -> System.out.println(l));
+
+        //driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("webdriver");
+        //driver.findElement(By.cssSelector("div.FPdoLc input[value='Поиск в Google']")).click();
     }
 
 }
